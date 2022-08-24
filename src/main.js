008e985cbbf2dd714dc3c7d7d6cee0bafe6b6801
2434 <cyuc@tongtai.com.tw>
2022-08-24 19:40:20 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "./App.vue"; //從 ./App.vue import 東西進來，然後叫他 App
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).mount("#app"); //建立APP，可以用store，router
