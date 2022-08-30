02ba32ab3a4fc83f7ace86e4b3ef8cb8c1c1edeb
2434 <cyuc@tongtai.com.tw>
2022-08-30 18:22:21 +0800

    vue cli3學習

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  base:'./',
+  transpileDependencies: [
+    'quasar'
+  ],
+
+  publicPath:'./',
+
+  pluginOptions: {
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: true
+    }
+  }
 });
